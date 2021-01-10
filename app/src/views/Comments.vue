bc64617ba53e0701a610f0894daad2a1cf64ec82
David Cha <djc991006@gmail.com>
2021-01-09 20:52:13 -0700

    Start new page for comments list

---
+++ app/src/views/Comments.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="menu">
+    <h2>Comments</h2>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/composition-api';
+import axios from 'axios';
+
+export default {
+  name: 'Comments',
+  setup() {
+
+    return {
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
